b47022113752726273af8cd4d8cbf5bd5beef532
Stéphane THOMAS <sthomas@campus-eni.fr>
2023-02-28 06:34:19 -0300

    Modif branche tool-tool

--- java/ClasseTest.java
+++ java/ClasseTest.java
@@ -2,5 +2,14 @@ public class ClasseTest{
 
 // Commentaire
 
+// Mes modifs dans l'espace de travail
+
+// git add . ==> prendre en compte les modifs pour la prochaine version
+
+// git commit -m "blablabla ==> Création de la version
+
+// git push  ==> envoyer les mise à jour vers le dépot distant github.com
+
+
 
 }
